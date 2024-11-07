2e893abffb7ba378512227cd1469b5a74f274ff7
Daniel F.Lima <danieldfl010492@gmail.com>
2024-11-07 11:51:04 -0300

    Refeito

--- ProjetoClubeCarros/src/components/BemVindo.jsx
+++ ProjetoClubeCarros/src/components/BemVindo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BemVindo = () => {
   return (
     <div className="container">
-      <h2>Bem-Vindo!</h2>
+      <h2>Bem-Vindos!</h2>
       <p>Você entrou com sucesso.</p>
     </div>
   );
