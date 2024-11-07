783d93ca6a87269e3ae9ade9967d4cbc5c24de74
ArthurSRods <arthur.srodrigues@hotmail.com>
2024-11-07 10:41:57 -0300

    Alteração inicial

--- ProjetoClubeCarros/src/components/BemVindo.jsx
+++ ProjetoClubeCarros/src/components/BemVindo.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const BemVindo = () => {
   return (
     <div className="container">
-      <h2>Bem-Vindo!</h2>
-      <p>Você entrou com sucesso.</p>
+      <h2>Bem-Vindo!!!</h2>
+      <p>Você entrou com sucesso!!!</p>
     </div>
   );
 };
