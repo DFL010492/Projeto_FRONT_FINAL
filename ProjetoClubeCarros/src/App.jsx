b7a7a71203bbce9bffe938783e0d43d4f777fbe0
Daniel F.Lima <danieldfl010492@gmail.com>
2024-11-11 16:10:19 -0300

    Adicionado a Pagina do formulario e arrumado pagina Home

--- ProjetoClubeCarros/src/App.jsx
+++ ProjetoClubeCarros/src/App.jsx
@@ -1,10 +1,10 @@
-// App.jsx
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Cadastro from './components/Cadastro';
 import Home from './components/Home';
+import MeuCarro from './components/MeuCarro';
 
 function App() {
   const [isRegistered, setIsRegistered] = useState(true);
@@ -24,11 +24,9 @@ function App() {
                 <Login onLoginSuccess={handleLoginSuccess} onToggleRegister={() => setIsRegistered(false)} />
               ) : (
                 <Cadastro onToggleRegister={() => setIsRegistered(true)} />
-              )
-            }
-          />
-          <Route path="/Home" element={<Home />} />
-          <Route path="*" element={<Navigate to="/" />} />
+              )}/>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/meucarro" element={<MeuCarro />}></Route>
         </Routes>
       </div>
     </Router>
